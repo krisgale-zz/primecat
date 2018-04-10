6b0556b89c0c8e619f96c79e1c4d79f484ddb7fb
krisgale <kgale4@gmail.com>
2018-04-10 20:40:21 +0000

    functions to render Primary Category sidebar metabox, for 'post' and all custom types that use 'category' taxonomy

--- primecat.php
+++ primecat.php
@@ -11,3 +11,37 @@ Author: krisgale
 Version: 1.0
 Author URI: krisgale.com
 */
+
+function primecat_meta_box_html( ) {
+    ?>
+    <select name="primecat_select">
+        <option value="">(None)</option>
+    <?php foreach( get_categories( [ 'hide_empty' => FALSE ] ) as $cat ) { ?>
+        <option value="<?php echo $cat->term_id; ?>"><?php echo $cat->name; ?></option>
+    <?php } ?>
+    </select>
+    <?php
+}
+
+function primecat_add_meta_box( ) {
+    $types = [ "post" ];
+    foreach( get_post_types( [ ], "objects" ) as $type ) {
+        if( property_exists( $type, "taxonomies" )
+        && in_array( "category", $type->taxonomies )
+        && !in_array( $type->name, $types ) ) {
+            $types[ ] = $type->name;
+        }
+    }
+    foreach( $types as $type ) {
+        add_meta_box(
+            "primecat_meta_box",
+            "Primary Category",
+            "primecat_meta_box_html",
+            $type,
+            "side",
+            "high"
+        );
+    }
+}
+
+add_action( "add_meta_boxes", "primecat_add_meta_box" );
